bb1511a4f727bd7c96858b441abc5ec7c78d00fd
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-07-21 01:32:55 +0530

    first commit

---
+++ src/main/java/com/scaler/productservicespringboot/ProductServiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/productservicespringboot/ProductServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
