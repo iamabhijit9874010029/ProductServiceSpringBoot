017bb50fb706d847c69bb0877420694e2db359a2
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-07-21 01:58:07 +0530

    added 2 models Category and Product with public access specifiers

---
+++ src/main/java/com/scaler/productservicespringboot/models/Category.java
@@ -0,0 +1,6 @@
+package com.scaler.productservicespringboot.models;
+
+public class Category {
+    public int id;
+    public String title;
+}
---
+++ src/main/java/com/scaler/productservicespringboot/models/Product.java
@@ -0,0 +1,10 @@
+package com.scaler.productservicespringboot.models;
+
+public class Product {
+    public int id;
+    public String name;
+    public String description;
+    public int price;
+    public String imgUrl;
+    public Category category;
+}
