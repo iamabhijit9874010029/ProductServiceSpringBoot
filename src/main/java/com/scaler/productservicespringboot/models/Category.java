a22e62634f1f5b9589da7e7b0e1dcb788627e34c
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-07-21 14:12:31 +0530

    annotations added for getter, setter, constructor

--- src/main/java/com/scaler/productservicespringboot/models/Category.java
+++ src/main/java/com/scaler/productservicespringboot/models/Category.java
@@ -1,5 +1,14 @@
 package com.scaler.productservicespringboot.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     private int id;
     private String title;
--- src/main/java/com/scaler/productservicespringboot/models/Product.java
+++ src/main/java/com/scaler/productservicespringboot/models/Product.java
@@ -1,74 +1,19 @@
 package com.scaler.productservicespringboot.models;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
     private int id;
     private String name;
     private String description;
     private int price;
     private String imgUrl;
     private Category category;
-
-    //constructors
-
-    public Product(int id, String name, String description, int price, String imgUrl, Category category) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.imgUrl = imgUrl;
-        this.category = category;
-    }
-
-    //getters
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    //setters
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
 }
