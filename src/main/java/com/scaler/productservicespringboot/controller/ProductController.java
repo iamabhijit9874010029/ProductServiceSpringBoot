0bf15580950990afd13a7f46733f3091aa22d32b
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-07-22 22:42:39 +0530

    added addProduct API (controller, service, DTOs)

--- src/main/java/com/scaler/productservicespringboot/controller/ProductController.java
+++ src/main/java/com/scaler/productservicespringboot/controller/ProductController.java
@@ -1,12 +1,11 @@
 package com.scaler.productservicespringboot.controller;
 
+import com.scaler.productservicespringboot.dto.FakeStoreRequestDTO;
 import com.scaler.productservicespringboot.models.Product;
 import com.scaler.productservicespringboot.services.FakeStoreProductService;
 import com.scaler.productservicespringboot.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,5 +26,11 @@ public class ProductController {
     public List<Product> getAllProducts(){
         List<Product> products = productService.getAllProducts();
         return products;
+    }
+
+    @PostMapping("/products")
+    public Product addProduct(@RequestBody FakeStoreRequestDTO fakeStoreRequestDTO){
+        Product saveProduct = productService.addProduct(fakeStoreRequestDTO);
+        return saveProduct;
     }
 }
--- src/main/java/com/scaler/productservicespringboot/services/ProductService.java
+++ src/main/java/com/scaler/productservicespringboot/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.scaler.productservicespringboot.services;
 
+import com.scaler.productservicespringboot.dto.FakeStoreRequestDTO;
 import com.scaler.productservicespringboot.models.Product;
 
 import java.util.List;
@@ -7,4 +8,10 @@ import java.util.List;
 public interface ProductService {
     Product getSingleProduct(int id);
     List<Product> getAllProducts();
+
+    //to create objects in your own db
+    Product addProduct(Product product);
+
+    //to create objects in fakestore db
+    Product addProduct(FakeStoreRequestDTO fakeStoreRequestDTO);
 }
