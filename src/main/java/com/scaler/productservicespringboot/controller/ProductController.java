e7b10cc8c0e2409529f7cc5b7c760a174b4d0671
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-07-24 02:12:50 +0530

    added responseEntity to implement proper status code

--- src/main/java/com/scaler/productservicespringboot/controller/ProductController.java
+++ src/main/java/com/scaler/productservicespringboot/controller/ProductController.java
@@ -9,6 +9,8 @@ import com.scaler.productservicespringboot.models.Product;
 import com.scaler.productservicespringboot.services.FakeStoreProductService;
 import com.scaler.productservicespringboot.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,31 +23,43 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping("/products/{id}")
-    public ProductResponseDTO getSingleProduct(@PathVariable("id") int id) {
+    public ResponseEntity<ProductResponseDTO> getSingleProduct(@PathVariable("id") int id) {
         try {
             Product product = productService.getSingleProduct(id);
             ProductResponseDTO productResponseDTO = new ProductResponseDTO();
             productResponseDTO.setProduct(product);
             productResponseDTO.setResponseMessage("Success");
-            return productResponseDTO;
+
+            ResponseEntity<ProductResponseDTO> responseEntity = new ResponseEntity<>(productResponseDTO, HttpStatus.OK);
+
+            return responseEntity;
         }
         catch (ProductNotFoundException pnfe) {
             ProductResponseDTO productResponseDTO = new ProductResponseDTO();
             productResponseDTO.setProduct(null);
             productResponseDTO.setResponseMessage(pnfe.getMessage()+" exception type - 1");
-            return productResponseDTO;
+
+            ResponseEntity<ProductResponseDTO> responseEntity = new ResponseEntity<>(productResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return responseEntity;
         }
         catch (DBNotFoundException dbnfe){
             ProductResponseDTO productResponseDTO = new ProductResponseDTO();
             productResponseDTO.setProduct(null);
             productResponseDTO.setResponseMessage(dbnfe.getMessage()+" exception type - 2");
-            return productResponseDTO;
+
+            ResponseEntity<ProductResponseDTO> responseEntity = new ResponseEntity<>(productResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return responseEntity;
         }
         catch (DBTimeOutException dbtoe){
             ProductResponseDTO productResponseDTO = new ProductResponseDTO();
             productResponseDTO.setProduct(null);
             productResponseDTO.setResponseMessage(dbtoe.getMessage()+" exception type - 3");
-            return productResponseDTO;
+
+            ResponseEntity<ProductResponseDTO> responseEntity = new ResponseEntity<>(productResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return responseEntity;
         }
     }
 
