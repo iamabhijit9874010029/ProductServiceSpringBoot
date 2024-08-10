d53605a9636f03f1e70d671159ea628f72e56a28
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-08-10 22:28:13 +0530

    telling spring that List<Product> products attribute in Category model/table is already handled in Product modlel/table with category column by using annotation OneToMany(mappedBy = 'category'

--- src/main/java/com/scaler/productservicespringboot/models/Category.java
+++ src/main/java/com/scaler/productservicespringboot/models/Category.java
@@ -2,12 +2,14 @@ package com.scaler.productservicespringboot.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -16,5 +18,8 @@ import java.util.Date;
 @Entity
 public class Category extends BaseModel{
     private String title;
-
+    //it is the inverse of the same relation between Product and Category
+    //telling spring that this attribute is already handled in product table with category coloumn
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
