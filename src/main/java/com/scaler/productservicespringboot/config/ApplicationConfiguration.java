ac2adcce66de23b388e8e48b2565c608b522439b
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-09-02 00:25:02 +0530

    in basemodel added EntityListeners, createdAt, updatedAt, AuditingEntityListener for make created, updated data generation

--- src/main/java/com/scaler/productservicespringboot/config/ApplicationConfiguration.java
+++ src/main/java/com/scaler/productservicespringboot/config/ApplicationConfiguration.java
@@ -2,9 +2,11 @@ package com.scaler.productservicespringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableJpaAuditing
 public class ApplicationConfiguration {
 
     @Bean
--- src/main/java/com/scaler/productservicespringboot/models/BaseModel.java
+++ src/main/java/com/scaler/productservicespringboot/models/BaseModel.java
@@ -1,20 +1,25 @@
 package com.scaler.productservicespringboot.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 import jakarta.persistence.Id;
+import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import jakarta.persistence.EntityListeners;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 
 import java.util.Date;
+
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,7 +39,13 @@ public class BaseModel {
 //    100
 //    last+random=>105
     private long id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt;
     private Boolean isDeleted;
 }
