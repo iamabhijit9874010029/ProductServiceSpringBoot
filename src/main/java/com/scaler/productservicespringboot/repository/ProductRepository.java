43acbc55033523f8c0443365992670045cf04498
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2024-09-02 18:48:14 +0530

    added HQL queries and SQL quries and test thorough test application - modified ProductRepository.java

--- src/main/java/com/scaler/productservicespringboot/repository/ProductRepository.java
+++ src/main/java/com/scaler/productservicespringboot/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.scaler.productservicespringboot.repository;
 import com.scaler.productservicespringboot.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -26,4 +27,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 //    SELECT p.name FROM products p JOIN category c ON p.category_id=c.id AND c.name=categoryName;
 
     List<Product> getProductsByCategoryTitle(String category);
+
+    //HQL query demo
+    @Query("SELECT p FROM Product p")
+    List<Product> giveEverythingFromProductTable();
+
+    //HQL query demo
+    @Query("SELECT p FROM Product p WHERE p.category.title=:catName")
+    List<Product> giveEverythingFromProductTableWhereCategoryNameIs(@Param("catName") String category);
+
+    //SQL query
+    @Query(value = "SELECT p.* FROM product AS p JOIN category AS c ON p.category_id=c.id WHERE c.title=:catName", nativeQuery = true)
+    List<Product> SQLqueryForgiveEverythingFromProductTableWhereCategoryNameIs(@Param("catName") String catName);
 }
--- src/test/java/com/scaler/productservicespringboot/ProductServiceSpringBootApplicationTests.java
+++ src/test/java/com/scaler/productservicespringboot/ProductServiceSpringBootApplicationTests.java
@@ -1,13 +1,50 @@
 package com.scaler.productservicespringboot;
 
+import com.scaler.productservicespringboot.models.Product;
+import com.scaler.productservicespringboot.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ProductServiceSpringBootApplicationTests {
+
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductServiceSpringBootApplicationTests(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void HQLqueryForGetEverything() {
+        List<Product> products = productRepository.giveEverythingFromProductTable();
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+    }
+
+    @Test
+    void HQLqueryForGetEverythingWhereCategory() {
+        List<Product> products = productRepository.giveEverythingFromProductTableWhereCategoryNameIs("laptop");
+
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+    }
+
+    @Test
+    void SQLqueryForGetEverythingWhereProduct() {
+        List<Product> products = productRepository.SQLqueryForgiveEverythingFromProductTableWhereCategoryNameIs("mobile");
+
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+    }
 }
